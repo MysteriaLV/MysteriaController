50faa6cbc447714695edea48439cba30aee462f2
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-01-24 23:21:53 +0200

    different firmata

--- lv/mysteria/firmata.py
+++ lv/mysteria/firmata.py
@@ -1,7 +1,6 @@
 import random
 
-from pymata_aio.constants import Constants
-from pymata_aio.pymata3 import PyMata3
+from pyfirmata2 import Arduino, Pin, INPUT
 
 
 class ZombieController(object):
@@ -14,26 +13,33 @@ class ZombieController(object):
 
     def __init__(self):
         self.main_quest = None
-        self.board = PyMata3()
+        self.board = Arduino(Arduino.AUTODETECT)
+        self.board.samplingOn(100)
 
-        self.board.set_pin_mode(self.PIN_BUTTON, Constants.ANALOG)
-        self.board.set_pin_mode(self.PIN_MIRROR, Constants.OUTPUT)
-        self.board.set_pin_mode(self.PIN_SPARKS, Constants.OUTPUT)
+        self.mirror_pin: Pin = self.board.digital[ZombieController.PIN_MIRROR]
+        self.sparks_pin: Pin = self.board.digital[ZombieController.PIN_SPARKS]
+
+        self.button_pin: Pin = self.board.analog[ZombieController.PIN_BUTTON]
+        self.button_pin.mode = INPUT
+        self.button_pin.register_callback(self.big_red_button_pressed)
+        self.button_pin.enable_reporting()
 
     def reset(self):
-        self.board.set_analog_latch(self.PIN_BUTTON, Constants.LATCH_GT, 99, self.big_red_button_pressed)
+        self.button_pin.enable_reporting()
 
     def mirror(self, turn_on=True):
-        self.board.digital_write(self.PIN_MIRROR, 0 if turn_on else 1)
+        self.mirror_pin.write(0 if turn_on else 1)
 
     def sparkle(self):
         for i in range(200):
-            self.board.digital_write(self.PIN_SPARKS, random.randint(0, 1))
-            self.board.sleep(0.02)
+            self.sparks_pin.write(random.randint(0, 1))
+            self.board.pass_time(0.02)
 
     def register_in_lua(self, main_quest):
         self.main_quest = main_quest
         return self
 
-    def big_red_button_pressed(data):
-        print(data)
+    def big_red_button_pressed(self, data):
+        if data == 1:
+            self.button_pin.disable_reporting()
+            print(data)
