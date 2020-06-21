13c71d5154ec77b00c19f5e711c3d8ae397db262
Pavels Veretennikovs <vermut@kid.lv>
2020-06-21 13:15:26 +0300

    feat: startup scripts

---
+++ start.cmd
@@ -0,0 +1,31 @@
+cd C:\MysteriaController
+
+set PYTHONUNBUFFERED=1
+set PYTHONPATH=C:\MysteriaController\lv
+
+set CommonProgramFiles=C:\Program Files\Common Files
+set HOMEDRIVE=C:
+set SystemDrive=C:
+set LOGONSERVER=\\DESKTOP-2KPO65I
+set TEMP=C:\Users\Mysteria\AppData\Local\Temp
+set TMP=C:\Users\Mysteria\AppData\Local\Temp
+set USERNAME=Mysteria
+set ProgramFiles=C:\Program Files
+set LOCALAPPDATA=C:\Users\Mysteria\AppData\Local
+set ProgramData=C:\ProgramData
+set APPDATA=C:\Users\Mysteria\AppData\Roaming
+set ALLUSERSPROFILE=C:\ProgramData
+set ComSpec=C:\WINDOWS\system32\cmd.exe
+set SystemRoot=C:\WINDOWS
+set Path=C:\Python37\Scripts\;C:\Python37\;C:\Python27\;C:\Python27\Scripts;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\ProgramData\chocolatey\bin;C:\Program Files\Java\jdk1.8.0_102\bin;C:\tools\python2-x86_32;C:\Program Files (x86)\Windows Live\Shared;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\Java\jdk1.8.0_172\bin;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Program Files\VideoLAN\VLC\;C:\Users\Mysteria\AppData\Local\Microsoft\WindowsApps;
+set ProgramFiles(x86)=C:\Program Files (x86)
+set USERPROFILE=C:\Users\Mysteria
+set windir=C:\WINDOWS
+set ProgramW6432=C:\Program Files
+set CommonProgramFiles(x86)=C:\Program Files (x86)\Common Files
+set PUBLIC=C:\Users\Public
+
+:run
+C:\Python37\python.exe C:\MysteriaController\lv\mysteria\app.py
+
+goto run
